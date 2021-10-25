95926f23d6f0037649023f4e9aa5563fb29a32e2
sh414lim <77113306+sh414lim@users.noreply.github.com>
2021-10-25 14:52:38 +0900

    setting

---
+++ leoposter/src/Pages/home.js
@@ -0,0 +1,11 @@
+import React,{Component} from "react";
+import styled from "styled-components";
+
+
+class Home extends Component{
+    render(){
+        return <h1>hello world2</h1>
+    }
+}
+
+export default Home;
